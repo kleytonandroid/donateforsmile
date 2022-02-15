81319cf8778a69d090df4a3b259ac0c6c2e6e2f0
Kleyton Santos <kleytonandroid@gmail.com>
2022-02-15 15:06:52 -0300

    Initial create, setup custom fonts, add imagens and colors

---
+++ Donate For Smile/Shared/Application/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Kleyton Santos on 07/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        FundraiserListView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Donate For Smile/Shared/Application/Donate_For_SmileApp.swift
@@ -0,0 +1,18 @@
+//
+//  Donate_For_SmileApp.swift
+//  Shared
+//
+//  Created by Kleyton Santos on 07/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Donate_For_SmileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.light)
+        }
+    }
+}
