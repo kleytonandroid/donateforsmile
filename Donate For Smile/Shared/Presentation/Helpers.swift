40db6c3aaf5ef5d5405378764590f4dbfeee9532
Kleyton Santos <kleytonandroid@gmail.com>
2022-02-15 15:10:52 -0300

    String for custom fonts and colors

---
+++ Donate For Smile/Shared/Presentation/Helpers.swift
@@ -0,0 +1,15 @@
+//
+//  Helpers.swift
+//  Donate For Smile
+//
+//  Created by Kleyton Santos on 07/02/22.
+//
+
+import Foundation
+
+let customFonts = "Metropolis-Regular"
+
+let appGreenColor = "appgreen"
+let appGrayColor = "appgray"
+let appBlackColor = "appblack"
+let appGrayBorderColor = "appgrayborder"
